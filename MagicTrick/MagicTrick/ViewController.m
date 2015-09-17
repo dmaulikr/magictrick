5fe287601bcdcb9687ff2a47b8a266f9ddde8cf4
Lucy Guo <lucy.guo@C17PRA2JG8WP.local>
2015-09-17 15:54:09 -0700

    more logic written out

--- MagicTrick/MagicTrick/ViewController.m
+++ MagicTrick/MagicTrick/ViewController.m
@@ -13,11 +13,13 @@
 @interface ViewController ()
 
 @property (nonatomic, strong) NSMutableArray *asymmetricalCards;
+@property (nonatomic, strong) NSMutableArray *symmetricalCards;
 @property (nonatomic, strong) NSMutableArray *faceCards;
 @property (nonatomic, strong) NSMutableArray *allCards;
 
 @property BOOL isFirstCardFace;
 @property BOOL cardsReset;
+@property NSInteger firstCardIndex;
 
 @property (nonatomic, strong) NSMutableArray *gameCards;
 
@@ -29,9 +31,6 @@
 
 - (id)init {
     self = [super init];
-    
-    _gameCards = [[NSArray alloc] init];
-    
     
     return self;
 }
@@ -55,42 +54,74 @@
             return NO;
         }
     }
+    _firstCardIndex = -1;
     return YES;
 }
 
-- (void)cardTapped {
+- (void)cardTapped:(NSInteger)index {
     _cardsReset = [self isAllCardsFaceDown];
     
     if (_cardsReset) {
-        [self chooseFirstCard];
+        [self chooseFirstCard:index];
     } else {
-        [self chooseCard];
+        [self chooseCard:index];
     }
 }
 
-- (void)chooseCard {
-    
+- (void)chooseCard:(NSInteger)index {
+    // first card chosen was a face, so all cards before must be symmetrical
+    if (index < _firstCardIndex && _isFirstCardFace) {
+        NSUInteger randomIndex = arc4random() % [_symmetricalCards count];
+        _gameCards[index] = _symmetricalCards[randomIndex];
+    }
+    // first card chosen was asymmetrical, so face cards or symmetrical cards work
+    else if (index < _firstCardIndex) {
+        // randomly choose face or symmetrical
+        if ([self thisWillFlipAQuarter]) {
+            NSUInteger randomIndex = arc4random() % [_symmetricalCards count];
+            _gameCards[index] = _symmetricalCards[randomIndex];
+        } else {
+            NSUInteger randomIndex = arc4random() % [_faceCards count];
+            _gameCards[index] = _faceCards[randomIndex];
+        }
+    }
+    // woohoo it's after the first index so we can choose any card!!
+    else {
+        NSUInteger randomIndex = arc4random() % [_allCards count];
+        _gameCards[index] = _allCards;
+    }
 }
 
 // only execute if cards are reset
-- (void)chooseFirstCard {
-    // lol janky random number
-    NSInteger randomNumber = arc4random() % 16;
-    if (randomNumber < 7) {
+- (void)chooseFirstCard:(NSInteger)index {
+    _firstCardIndex = index;
+    
+    if ([self thisWillFlipAQuarter]) {
         _isFirstCardFace = YES;
         
         // set first card to random face card
         NSUInteger randomIndex = arc4random() % [_faceCards count];
-        _gameCards[0] = _faceCards[randomIndex];
+        _gameCards[_firstCardIndex] = _faceCards[randomIndex];
         
     } else {
         _isFirstCardFace = NO;
         
         // set first card to random asymmetrical card
         NSUInteger randomIndex = arc4random() % [_asymmetricalCards count];
-        _gameCards[0] = _faceCards[randomIndex];
+        _gameCards[_firstCardIndex] = _faceCards[randomIndex];
     }
     
+}
+
+// it's like flipping a quarter. lol.
+- (BOOL)thisWillFlipAQuarter {
+    int x = arc4random() % 2;
+    
+    if (x == 1) {
+        return YES; // 25% of the time you get 0
+    } else {
+        return NO; // the other 25%, when you get 1
+    }
 }
 
 - (void)viewDidLoad {
