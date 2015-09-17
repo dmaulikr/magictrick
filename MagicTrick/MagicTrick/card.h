122a7d4b21fe3cbc82b8a89ff574f0044e983d7c
Lucy Guo <lucy.guo@C17PRA2JG8WP.local>
2015-09-17 15:02:06 -0700

    card model

---
+++ MagicTrick/MagicTrick/card.h
@@ -0,0 +1,17 @@
+//
+//  cardView.h
+//  MagicTrick
+//
+//  Created by Lucy Guo on 9/17/15.
+//  Copyright (c) 2015 Lucy Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface card ()
+
+@property (nonatomic, strong) UIImageView *cardView;
+@property (nonatomic, strong) NSInteger *value;
+@property (nonatomic, strong) BOOL *flipped;
+
+@end
---
+++ MagicTrick/MagicTrick/card.m
@@ -0,0 +1,14 @@
+//
+//  cardView.m
+//  MagicTrick
+//
+//  Created by Lucy Guo on 9/17/15.
+//  Copyright (c) 2015 Lucy Guo. All rights reserved.
+//
+
+#import "card.h"
+
+@implementation card
+
+
+@end
